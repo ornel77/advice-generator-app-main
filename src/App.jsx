10da4cac31ac67baad8fd57d3ceaf74ac48199b1
ornel77 <t.ornel@yahoo.fr>
2023-05-16 23:41:15 +0200

    app static for mobile ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import './App.css'
-// import generateBtn from '../src/assets/icon-dice.svg'
-
+import './App.css';
+import generateBtn from '../src/assets/icon-dice.svg';
+import dividerMobile from '../src/assets/pattern-divider-mobile.svg';
 
 function App() {
-
   return (
-    <div>
-      bonjour
+    <div className='advice-container'>
+      <h3 className='advice-id'>advice #117</h3>
+      <p className='advice'>
+        &quot;It is easy to sit up and take notice, what&apos;s difficult is
+        getting up and taking action.&quot;
+      </p>
+      <img src={dividerMobile} alt='' className='divider-mobile' />
+      <button className='generate-btn'>
+        <img src={generateBtn} alt='' className='generate-btn-svg' />
+      </button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
