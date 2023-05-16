8ab37ba7016a422b00a07f01cd739266e57b4f04
ornel77 <t.ornel@yahoo.fr>
2023-05-17 01:33:04 +0200

    fetch ok but a bug, desktop ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,42 @@
 import './App.css';
 import generateBtn from '../src/assets/icon-dice.svg';
 import dividerMobile from '../src/assets/pattern-divider-mobile.svg';
+import dividerDesktop from '../src/assets/pattern-divider-desktop.svg';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [adviceObj, setAdviceObj] = useState(false)
+  useEffect(() => {
+    fetch('https://api.adviceslip.com/advice')
+      .then(response => response.json())
+      .then(data => setAdviceObj(data.slip))
+      .catch(e => console.error(e)) 
+  }, [])
+
+  const hancleClick = async () => {
+    
+    fetch('https://api.adviceslip.com/advice')
+      .then(response => response.json())
+      .then(data => {
+        setAdviceObj(data.slip)
+        console.log("result is ",data.slip);
+      })
+      .catch(e => console.error(e)) 
+  
+    console.log(adviceObj);
+  };
+  
+
   return (
+    
     <div className='advice-container'>
-      <h3 className='advice-id'>advice #117</h3>
+      <h3 className='advice-id'>advice #{adviceObj &&adviceObj?.id}</h3>
       <p className='advice'>
-        &quot;It is easy to sit up and take notice, what&apos;s difficult is
-        getting up and taking action.&quot;
+        &quot;{adviceObj?.advice}&quot;
       </p>
       <img src={dividerMobile} alt='' className='divider-mobile' />
-      <button className='generate-btn'>
+      <img src={dividerDesktop} alt="" className="divider-desktop" />
+      <button className='generate-btn' onClick={hancleClick}>
         <img src={generateBtn} alt='' className='generate-btn-svg' />
       </button>
     </div>
